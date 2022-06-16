66615ddb8d7f59f530c3d7e61354fb26e440dfc9
Valentin Nikolov <valentin.nikolov@lab08.com>
2022-06-16 13:22:35 +0300

    demo pull request

--- src/main/java/org/example/twelve/Student.java
+++ src/main/java/org/example/twelve/Student.java
@@ -29,6 +29,7 @@ public class Student implements Serializable {
         System.out.println("Registration Number: " + this.regNo);
         System.out.println("Is Hosteller: " + this.isHosteller);
         System.out.println("Serial version UID: " + serialVersionUID);
+//        System.out.println("Serial version UID: " + serialVersionUID);
     }
 }
 
