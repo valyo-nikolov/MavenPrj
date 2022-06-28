83f8e57401340ff8c8170444d825e18630f6622b
Valentin Nikolov <valentin.nikolov@lab08.com>
2022-06-28 22:20:33 +0300

    added more tests

--- src/test/java/api/tests/RestAssuredDemoTest.java
+++ src/test/java/api/tests/RestAssuredDemoTest.java
@@ -1,10 +1,12 @@
 package api.tests;
 
+import com.google.gson.JsonObject;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.example.common.Constants;
 import org.json.simple.JSONObject;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
@@ -14,6 +16,8 @@ import org.testng.annotations.Test;
 import java.security.SecureRandom;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -130,6 +134,115 @@ public class RestAssuredDemoTest {
         ArrayList<Object> listOfUsers = response.jsonPath().get();
 
         Assert.assertFalse(listOfUsers.isEmpty());
+    }
+
+    @Test(dependsOnGroups = "signup")
+    public void testLogout() {
+        Response response = given()
+                .log()
+                .all()
+                .header("Authorization", accessToken)
+                .when()
+                .delete("/users/logout")
+                .then().log().all()
+                .extract()
+                .response();
+
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode, HttpStatus.SC_OK);
+        Assert.assertEquals(response.jsonPath().get("msg"), "User successfully logged out.");
+    }
+
+    @Test
+    public void testGetPosts() {
+        int numberOfPosts = 3;
+        int numberOfSkippedPosts = 0;
+
+        Response response = given()
+                .queryParam("take", numberOfPosts)
+                .queryParam("skip", numberOfSkippedPosts)
+                .log()
+                .all()
+                .when()
+                .get("/posts")
+                .then().log().all()
+                .extract()
+                .response();
+
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode, HttpStatus.SC_OK);
+
+        List<LinkedHashMap<Object, Object>> posts = response.jsonPath().get();
+        Assert.assertEquals(posts.size(), numberOfPosts);
+
+        posts.forEach(post -> {
+            Assert.assertTrue(post.get("postStatus").toString().contentEquals("private") ||
+                    post.get("postStatus").toString().contentEquals("public"));
+        });
+    }
+
+    @Test(dependsOnGroups = "signup")
+    public void testCreatePublicPost() {
+        String caption = "test public post";
+        String coverUrl = "https://i.imgur.com/zShpZot.jpg";
+        String postStatus = "public";
+
+        JSONObject body = new JSONObject();
+        body.put("caption", caption);
+        body.put("coverUrl", coverUrl);
+        body.put("postStatus", postStatus);
+
+        Response response = given()
+                .log()
+                .all()
+                .header("Authorization", accessToken)
+                .contentType(ContentType.JSON)
+                .when()
+                .body(body)
+                .post("/posts")
+                .then().log().all()
+                .extract()
+                .response();
+
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode, HttpStatus.SC_CREATED);
+        Assert.assertNotNull(response.jsonPath().get("id"));
+        Assert.assertEquals(response.jsonPath().get("postStatus"), postStatus);
+        Assert.assertEquals(response.jsonPath().get("coverUrl"), coverUrl);
+        Assert.assertEquals(response.jsonPath().get("caption"), caption);
+        Assert.assertEquals(response.jsonPath().get("user.username"), username);
+    }
+
+    @Test(dependsOnGroups = "signup")
+    public void testCreatePrivatePost() {
+        String caption = "test public post";
+        String coverUrl = "https://i.imgur.com/zShpZot.jpg";
+        String postStatus = "private";
+
+        JSONObject body = new JSONObject();
+        body.put("caption", caption);
+        body.put("coverUrl", coverUrl);
+        body.put("postStatus", postStatus);
+
+        Response response = given()
+                .log()
+                .all()
+                .header("Authorization", accessToken)
+                .contentType(ContentType.JSON)
+                .when()
+                .body(body)
+                .post("/posts")
+                .then().log().all()
+                .extract()
+                .response();
+
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode, HttpStatus.SC_CREATED);
+        Assert.assertNotNull(response.jsonPath().get("id"));
+        Assert.assertEquals(response.jsonPath().get("postStatus"), postStatus);
+        Assert.assertEquals(response.jsonPath().get("coverUrl"), coverUrl);
+        Assert.assertEquals(response.jsonPath().get("caption"), caption);
+        Assert.assertEquals(response.jsonPath().get("user.username"), username);
     }
 
 }
